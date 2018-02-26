3fc3b96c44a7efcd8692e26ac23d512dd1c552f8
jacksona <jacksona@google.com>
2018-02-26 15:37:38 -0800

    use a dumb cache

--- web/serve.py
+++ web/serve.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0, '.')  # shitty hack.
 
 from flask import Flask
+from werkzeug.contrib.cache import SimpleCache
 from flask import Flask, request, render_template, redirect, abort, url_for
 
 from tensorflow import gfile
@@ -10,19 +11,29 @@ import os
 import rl_loop
 
 app = Flask(__name__)
+cache = SimpleCache()  # TODO(amj): replace with memcached
+
+
+EVAL_DIR = os.path.join(rl_loop.BASE_DIR, 'eval')
 
 
 @app.route('/')
 def index():
-    models = rl_loop.get_models()
-    print(models)
+    models = cache.get('models')
+    if models is None:
+        models = rl_loop.get_models()
+        cache.set('models', models, timeout=10*60)
     return render_template("index.html", models=[m[1] for m in models])
 
 
 @app.route("/model/<path:model_name>")
 def model_list(model_name):
-    games = gfile.Glob(os.path.join(
-        rl_loop.SGF_DIR, model_name, 'clean', '*.sgf'))
+    games = cache.get(model_name)
+    if games is None:
+        games = gfile.Glob(
+            os.path.join(rl_loop.SGF_DIR, model_name, 'clean', '*.sgf')
+        )
+        cache.set(model_name, games, timeout=5*60)
     return render_template("model.html", files=games, model=model_name)
 
 
