5a2e8f992b5c2df67559f7569adbee6140afc47f
jacksona <jacksona@google.com>
2019-03-28 12:05:47 -0700

    comments

--- ml_perf/repeat_run.sh
+++ ml_perf/repeat_run.sh
@@ -1,4 +1,5 @@
-# Copyright 2018 Google LLC
+#!/bin/bash
+# Copyright 2019 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash
+# Usage:  `repeat_run.sh bucket_name` where 'bucket-name' is the path to a gcs
+#         bucket without the gs:// prefix
 
 set -e
-
 : ${1?"Need to call with 'repeat_run.sh BUCKET_NAME' (no gs:// prefix needed)"}
 
 BUCKET_NAME=$1
 
 while true
 do
-  BASE_DIR=$(pwd)/results/$(hostname)-$(date +%Y-%m-%d-%H)
+  BASE_DIR=$(pwd)/results/$(hostname)-$(date +%Y-%m-%d-%H-%M)
   BOARD_SIZE=9 python ml_perf/reference_implementation.py \
       --base_dir=$BASE_DIR \
       --flagfile=ml_perf/flags/9/rl_loop.flags \
@@ -32,7 +33,7 @@ do
       --base_dir=$BASE_DIR \
       --flags_dir=ml_perf/flags/9/
 
-  gsutil -m rsync -r -x "data" $BASE_DIR gs://$1/$(basename $BASE_DIR)
+  gsutil -m rsync -r -x "data" $BASE_DIR gs://$BUCKET_NAME/$(basename $BASE_DIR)
 done
 
 
