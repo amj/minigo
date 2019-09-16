08f36b6da784bf9766d2c6ff34737ed58ea73379
Andrew Jackson <jacksona@google.com>
2019-09-16 23:53:11 +0000

    helper utils

---
+++ ml_perf/launch_5.sh
@@ -0,0 +1,8 @@
+
+WORK_DIR=$1
+export BOARD_SIZE=9 
+for i in {1..5}; do
+  D=$WORK_DIR$i
+  echo $D
+python ml_perf/reference_implementation.py --bootstrap --flagfile=ml_perf/flags/9/rl_loop.flags --base_dir=$D 
+done
---
+++ oneoffs/dump_last.sh
@@ -0,0 +1,36 @@
+
+
+WORK_DIR=$1
+
+while true; do
+LAST=`ls -I '*target*' $WORK_DIR/sgf/eval | tail -n 1`
+#LAST=`ls $WORK_DIR/sgf/eval | tail -n 1`
+
+F=`ls $WORK_DIR/sgf/eval/$LAST | grep "\-50\-" - `
+if [ ! -f "$WORK_DIR/sgf/eval/$LAST/$F" ]; then
+	echo "Waiting for games in $LAST..."
+	sleep 10
+fi 
+python oneoffs/dump_game.py $WORK_DIR/sgf/eval/$LAST/$F
+sleep 1
+F=`ls $WORK_DIR/sgf/eval/$LAST | grep "\-30\-" - `
+python oneoffs/dump_game.py $WORK_DIR/sgf/eval/$LAST/$F
+sleep 1
+F=`ls $WORK_DIR/sgf/eval/$LAST | grep "\-80\-" - `
+python oneoffs/dump_game.py $WORK_DIR/sgf/eval/$LAST/$F
+sleep 1
+F=`ls $WORK_DIR/sgf/eval/$LAST | grep "\-99\-" - `
+python oneoffs/dump_game.py $WORK_DIR/sgf/eval/$LAST/$F
+
+echo $LAST
+
+while : 
+do
+		NEXT=`ls -I '*target*' $WORK_DIR/sgf/eval | tail -n 1`
+		if [ $NEXT != $LAST ] ; then
+				break
+		fi
+		echo "Waiting... Last model seen was $NEXT"
+		sleep 15
+done
+done
